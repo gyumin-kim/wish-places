05a0942f42aa3ab1f55790f999e2fe9c5cef32bd
Gyumin Kim <starkying@gmail.com>
2019-01-26 15:16:55 +0900

    onClickCounter / 'name' state 변경

--- src/App.js
+++ src/App.js
@@ -1,27 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const Person = (props) => {
+  return (
+    <p>
+      {props.name}: {props.count}
+    </p>
+  );
+};
 
 class App extends Component {
+  state = {
+    gyumin: 0,
+    kim: 0,
+    web: 0,
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Person name="Gyumin" count={this.state.gyumin}/>
+        <button type="button" onClick={this.onClickCounter.bind(this)} name="gyumin">
+          Gyumin Up
+        </button>
+
+        <Person name="Kim" count={this.state.kim}/>
+        <button type="button" onClick={this.onClickCounter.bind(this)} name="kim">
+          Kim Up
+        </button>
+
+        <Person name="Web" count={this.state.web}/>
+        <button type="button" onClick={this.onClickCounter.bind(this)} name="web">
+          Web Up
+        </button>
       </div>
     );
+  }
+  
+  onClickCounter(event) {
+    // name: <button>의 name property가 들어온다.
+    const { name } = event.target;
+    // const name = event.target.name;
+
+    this.setState({
+      // 동적으로 key를 처리. name으로 들어온 것에 따라 다른 state를 처리
+      [name]: this.state[name] + 1,
+    });
   }
 }
 
